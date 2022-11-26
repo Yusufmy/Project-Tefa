99bd8cb2b6aeacdf08c0ef96b9682059fc6581b4
Yusufmu <maulanayusuf@smkwikrama.sch.id>
2022-11-26 10:21:00 +0700

    pembaruan tampilan & menambahkan komponen

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -14,45 +14,193 @@
     <title>Project | Tefa</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+{{-- Navbar --}}
+    <nav class="navbar navbar-expand-lg navbar-dark"
+    style="background-color: #050f2c;">
       <div class="container">
-        <img src="assest\img\imgSMK.png" style="width: 80px">
+        <img src="assest/img/imgSMK.png" width="80px">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto" >
+          <ul class="navbar-nav ms-auto" style="padding: 20px">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Tentang</a>
+              <a class="nav-link active" aria-current="page" href="#active">Tentang</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Produk & Jasa</a>
+              <a class="nav-link" href="#">Produk</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Industry</a>
+              <a class="nav-link" href="#">Team</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Hubungi Kami</a>
+              <a class="nav-link" href="#">Industri</a>
             </li>
           </ul>
-         <div class="navbar-nav ms-auto">
-        <button type="button" class="btn btn-primary"><i class="bi bi-box-arrow-in-right"></i>Logout</button>
-      </div>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#getstart">Getstart</a>
+          </li>
         </div>
       </div>
     </nav>
-  <div class="img" style="position: absolute; margin-top: 97px">
-        <img src="assest/img/background.jpg" alt="" width="1349px">
-  </div>
-  <div class="img-background" style="position:absolute;margin-top: 535px">
-    <img src="assest/img/background2.jpg" alt="" width="1349px">
-  </div>
-  <div class="componen1" style="position: absolute; margin-top:86px; margin-left:849px">
-    <img src="assest/img/componen1.png" alt="" width="500px">
-  </div>
-  <div class="componen2" style="position: absolute; margin-top:570px; margin-left:15px">
-    <img src="assest/img/componen2.png" alt="" width="400px">
-  </div>
-    <div class="container">
-       @yield('container')
+{{-- Akhir Navbar --}}
+
+{{-- Jumbotron --}}
+<section id="getstart">
+    <div class="jumbotron">
+      <div class="container">
+        <div class="image" style="position: absolute;background-size:cover; background-repeat: no-repeat;
+        margin-left: 732px; margin-top:20px">
+          <img src="assest/img/componen1.png" width="500px">
+        </div>
+         <h1 class="display-4">Teaching Factory</h1>
+         <div class="col-7 mt-3 ms-2 mb-3">
+              <p class="lead" style="font-size:25px">The learning model in SMK is production/service based which refers to the standards and procedures that apply in the industry and is carried out in an atmosphere like what happens in the industry.</p>
+         </div>
+           <div class="col">
+              <div class="icon" style="margin-left: 500px;margin-top:170px">
+                <i class="bi bi-whatsapp"></i>
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-envelope"></i>
+              </div>
+          </div>
+       </div> 
     </div>
+</section>
+{{-- Akhir Jumbotron --}}
+
+{{-- Activity TEFA --}}
+<section id="active">
+    <div class="componen2" style="position: absolute;">
+      <img src="assest/img/componen2.png" width="400px">
+    </div>
+       <div class="container">
+          <div class="row">
+              <div class="col text-white mb-3">  
+                 <h2 style="margin-left: 30%">Activity TEFA</h2>
+                 <p style="margin-left: 30%">The activities carried out are receiving orders, analyzing orders, stating readiness to work on orders,
+                  take orders, evaluate products, and submit orders.</p>
+              </div>
+          </div>
+       </div>
+</section>
+{{-- Akhir activity TEFA--}}
+
+{{-- Goal TEFA --}}
+<section id="goal">
+  <div class="container">
+    <div class="row text-end">
+      <div class="col mt-2 mb-3">
+        <h2>Goal TEFA</h2>
+        <p>For learning activities that allow students to<br>
+           learn to produce goods in accordance with their disciplines and do business.</p>
+       </div>
+    </div>
+  </div>
+</section>
+{{-- Akhir Goal TEFa --}}
+
+{{-- Benefit TEFA --}}
+<section id="benefit">
+  <div class="container">
+    <div class="row">
+      <div class="col text-white mb-5">
+          <h2>Benefit TEFA</h2>
+          <ul>
+            <li><img src="assest/img/componen1/1.png" alt="">ㅤProvide skills needed in the world of work,</li>
+          </ul>
+          <ul>
+            <li><img src="assest/img/componen1/2.png" alt="">ㅤExpanding the scope of recruitment opportunities for SMK graduates,</li>
+          </ul>
+          <ul>
+            <li><img src="assest/img/componen1/3.png" alt="">ㅤAssisting vocational students in preparing themselves to become the workforce,</li>
+          </ul>
+          <ul>
+            <li><img src="assest/img/componen1/4.png" alt="">ㅤGenerate profits to support KBM activities.</li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</section>
+{{-- Akhir benefit TEFA --}}
+
+{{-- Produk Kejuruan --}}
+ <section id="portfolio" class="portfolio">
+ <div class="container">
+  <div class="row mt-5 text-center">
+    <div class="col">
+      <h2>Major Product</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card">
+        <img src="assest/img/produk/portfolio-1.png" class="card-img-top" alt="projek 1">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img src="assest/img/produk/portfolio-1.png" class="card-img-top" alt="projek 1">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img src="assest/img/produk/portfolio-1.png" class="card-img-top" alt="projek 1">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img src="assest/img/produk/portfolio-1.png" class="card-img-top" alt="projek 1">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img src="assest/img/produk/portfolio-1.png" class="card-img-top" alt="projek 1">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img src="assest/img/produk/portfolio-1.png" class="card-img-top" alt="projek 1">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+ </div>
+ </section>
+{{-- Akhir produk kejuruan --}}
+<div class="container">
+  @yield('container')
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
